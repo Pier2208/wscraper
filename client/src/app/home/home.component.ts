1531c11815bc68b0cd716843fd37b780c2ae8d67
Pier2208 <p.leroy.mtl@gmail.com>
2022-02-04 11:06:11 -0500

    ajout interface Job et Url dans HomeComponent

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { JobService } from '../services/job.service';
 
+interface Url {
+  _id: string;
+  url: string;
+  status: string;
+}
+
+interface Job {
+  _id: string;
+  name: string;
+  status: string;
+  count: number;
+  urls: Url[];
+  createdAt: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  jobs: Job[] = [];
   constructor(private job: JobService) {}
 
   ngOnInit(): void {
