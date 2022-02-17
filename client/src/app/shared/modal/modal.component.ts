ef374d9516a73664052a482e1499f3e1fec40f4a
Pier2208 <p.leroy.mtl@gmail.com>
2022-02-17 13:39:38 -0500

    config modal/overlay + open/close modal fonctionnalities

--- client/src/app/shared/modal/modal.component.ts
+++ client/src/app/shared/modal/modal.component.ts
@@ -4,13 +4,14 @@ import { ModalService } from 'src/app/services/modal.service';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
+  constructor(public modal: ModalService) {}
 
-  constructor(public modal: ModalService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  closeModal() {
+    this.modal.toggleModal();
   }
-
 }
